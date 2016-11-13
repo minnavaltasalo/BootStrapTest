2a682183427a89c37ce2104dd8eb3e0387236817
Minna Valtasalo <minna.valtasalo@stud.pointcollege.fi>
2016-11-13 17:53:50 +0200

    Projekti luotu

---
+++ BootstrapWebApp/BootstrapWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapWebApp.Startup))]
+namespace BootstrapWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
